610fdf731f8c520c13eff54b7744b3acd816c753
Pascal Lee Slew <pascal.leeslew@mail.utoronto.ca>
2022-04-26 23:57:58 -0400

    final update on the paper with intro, data, model, results, discussion and datasheet

--- scripts/01-data_cleaning.R
+++ scripts/01-data_cleaning.R
@@ -1,15 +1,14 @@
 #### Preamble ####
 # Purpose: The purpose of this code is to clean up     Clean the survey data downloaded from [...UPDATE ME!!!!!]That data is available to U of T students, but it needs 
 # to be put into a tidy format before it can be analysed.
-# Author: Pascal Lee Slew [CHANGE THIS TO YOUR NAME!!!!]
-# Data: 29 April 2022
+# Author: Pascal Lee Slew
+# Date: 29 April 2022
 # Contact: pascal.leeslew@mail.utoronto.ca 
 # License: MIT
 # Pre-requisites: 
-# - Need to have downloaded the ACS data and saved it to inputs/data
-# - Don't forget to gitignore it!
-# - Change these to yours
-# Any other information needed?
+# - Need to have downloaded the CCHS data and saved it to inputs/data
+# - Gitignore it
+
 
 
 #### Workspace setup ####
@@ -17,15 +16,15 @@
 library(haven)
 library(tidyverse)
 library(janitor)
-
+library(readr)
 # Read in the raw data. 
-raw_data <- readr::read_csv("inputs/data/raw_data.csv")
+#raw_data <- readr::read_csv("inputs/data/cchs.csv")
 
 # Load the data dictionary and the raw data and correct the variable names
-raw_data <- read_csv("  .csv")
-dict <- read_lines(" _dict.txt", skip = number ) # skip is because of preamble content
+raw_data <- read_csv("inputs/data/cchs.csv")
+dict <- read_lines("inputs/data/cchs_dict.txt", skip = 18) # skip is because of preamble content
 # Now we need the labels because these are the actual responses that we need
-labels_raw <- read_file("  _labels.txt")
+labels_raw <- read_file("inputs/data/chhs_labels.txt")
                      
 
 ### Setup the dictionary ###
@@ -87,25 +86,59 @@ cw_statements <-
 #### Apply that dictionary to the raw data ####
 # Pull out a bunch of variables and then apply the case when statement for the categorical variables
 
-cchs <- raw_data %>% 
-  select()
+cchs_data <- raw_data %>% 
+  select(CASEID,
+         slp_010,
+         slp_015,
+         slp_020,
+         dhhgage,
+         dhhgms,
+         dhh_sex,
+         slpg005,
+         ccc_035,
+         ccc_195,
+         ccc_200,
+         depdvsev,
+         incdgper,
+         gen_005,
+         gen_015,
+         gen_020,
+         gen_025,
+         gen_010) %>% 
+  mutate_at(vars(slp_010:gen_010), .funs = funs(ifelse(.>=96, NA, .))) %>% 
+  mutate_at(.vars = vars(slp_010:gen_025),
+            .funs = funs(eval(parse(text = cw_statements %>%
+                                      filter(variable_name==deparse(substitute(.))) %>%
+                                      select(cw_statement) %>%
+                                      pull()))))
+
+# Fix the names
+cchs_data1 <- cchs_data %>% 
+  clean_names() %>% 
+  rename(caseid = caseid,
+         diff_sleep_or_stay_asleep = slp_010,
+         refreshing_sleep = slp_015,
+         diff_stay_awake = slp_020,
+         age = dhhgage,
+         marital_status = dhhgms,
+         sex = dhh_sex,
+         num_hrs_sleep = slpg005,
+         sleep_apnea = ccc_035,
+         mood_disorder = ccc_195,
+         anxiety_disorder = ccc_200,
+         depression_scale = depdvsev,
+         perceived_health = gen_005,
+         life_satisfaction = gen_010,
+         per_mental_health = gen_015,
+         per_life_stress = gen_020,
+         per_stress_work = gen_025,
+         income = incdgper)
 
 
+# save file
+write_csv(cchs_data1, "outputs/paper/cchs_data1.csv")
 
 
-
-# Just keep some variables that may be of interest (change 
-# this depending on your interests)
-names(raw_data)
-
-reduced_data <- 
-  raw_data %>% 
-  select(first_col, 
-         second_col)
-rm(raw_data)
-         
-
-#### What's next? ####
 
 
 
